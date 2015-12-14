f8b0b69090a8e5ec4a82090c10859d606ffe71c5
kratcliff15 <kratcliff15@my.whitworth.edu>
2015-12-14 10:07:18 -0800

    Road structure

--- TrafficSim/Road.h
+++ TrafficSim/Road.h
@@ -1,1 +1,54 @@
-#pragma once
+#include <iostream>
+#include <queue>
+#include "Resident.h"
+
+
+
+class Road
+{
+private:
+	std::queue<Resident*> drivers;
+	int length;
+	int capacity;
+	virtual Resident * pop();
+public:
+	Road();
+	~Road();
+	virtual void update(int clock);
+};
+
+class ResidentialRoad :public Road
+{
+private:
+	Resident * pop();
+	void reserve();
+public:
+	ResidentialRoad();
+	~ResidentialRoad();
+	void update(int clock);
+
+};
+
+class EntryRoad : public Road
+{
+private:
+	ResidentialRoad* sourceRoad;
+	Resident * pop();
+
+public:
+	EntryRoad();
+	~EntryRoad();
+	void update(int clock);
+
+};
+
+class DestRoad :public Road
+{
+private:
+	EntryRoad* source;
+	Resident * pop();
+public:
+	DestRoad();
+	~DestRoad();
+	void update();
+};
---
+++ TrafficSim/roads.cpp
@@ -0,0 +1,82 @@
+#include "Road.h"
+
+
+
+Road::Road()
+{
+}
+
+Road::~Road()
+{
+}
+
+
+
+ResidentialRoad::ResidentialRoad()
+{
+}
+
+ResidentialRoad ::~ResidentialRoad()
+{
+}
+
+void ResidentialRoad::update(int clock)
+{
+
+}
+
+Resident * ResidentialRoad::pop()
+{
+	//update wait time etc
+
+}
+
+void ResidentialRoad::reserve()
+{
+	//increase capacity of road as necessary
+}
+
+void ResidentialRoad::update(int clock)
+{
+
+}
+
+EntryRoad::EntryRoad()
+{
+
+}
+
+EntryRoad :: ~EntryRoad()
+{
+
+}
+
+Resident * EntryRoad::pop()
+{
+
+}
+
+void EntryRoad::update(int clock)
+{
+
+}
+
+DestRoad::DestRoad()
+{
+
+}
+
+DestRoad:: ~DestRoad()
+{
+
+}
+
+Resident * DestRoad::pop()
+{
+
+}
+
+void DestRoad::update(int clock)
+{
+
+}
